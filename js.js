da17b8221a577f9c0d7a6f45e3c67052384b9cfe
Greg Schneider <greg53x@msn.com>
2022-11-16 23:45:23 -0700

    function to render game status onto board

--- js.js
+++ js.js
@@ -1,3 +1,14 @@
-gameBoard = () =>{
-    gameStatus = ['X','O','X','O','X','O','X','O','X']
-}
+gameBoard = (() =>{
+    let gameStatus = ['X','O','X','O','X','O','X','O','X'];
+    
+    const renderGame = () =>{
+        
+        for (let i=0; i<9; i++){
+            document.querySelector(`#cell${i+1}`).textContent = gameStatus[i];
+        }
+    };
+    renderGame();
+    return {
+        renderGame
+    }
+})();
