47d53f20c7851a94a666f7a95aef6043f0a644f7
Greg Schneider <greg53x@msn.com>
2022-11-17 00:24:10 -0700

    instead of passing status array, new function to update gameStatus

--- js.js
+++ js.js
@@ -1,19 +1,24 @@
 const gameBoard = (() =>{
     let gameStatus = ['X','O','X','O','X','O','X','O','X'];
     
-    const renderGame = (gameStatus) =>{
+    const renderGame = () =>{
         
         for (let i=0; i<9; i++){
             document.querySelector(`#cell${i+1}`).textContent = gameStatus[i];
         }
         return 'test';
     };
-    renderGame(gameStatus);
-    
+    renderGame();
+    const updateGameStatus = (newArray) =>{
+        gameStatus = newArray;
+    }
     return {
         renderGame,
-        gameStatus,
+        updateGameStatus,
     }
 })();
 // gameBoard.gameStatus = [1,1,1,1,1,1,1,1,1]
-gameBoard.renderGame([1,1,1,1,1,1,1,1,1]);
+// gameBoard.renderGame([1,1,1,1,1,1,1,1,1]);
+
+// https://www.google.com/search?client=firefox-b-1-d&q=js+change+variable+inside+iife 
+// read this ^^^
