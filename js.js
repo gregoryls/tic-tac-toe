1111c8c425be8c9f01b707776020f3b342ea03f6
Greg Schneider <greg53x@msn.com>
2022-11-17 00:16:40 -0700

    pass status array to render function directly

--- js.js
+++ js.js
@@ -1,15 +1,19 @@
-gameBoard = (() =>{
+const gameBoard = (() =>{
     let gameStatus = ['X','O','X','O','X','O','X','O','X'];
     
-    const renderGame = () =>{
+    const renderGame = (gameStatus) =>{
         
         for (let i=0; i<9; i++){
             document.querySelector(`#cell${i+1}`).textContent = gameStatus[i];
         }
+        return 'test';
     };
-    renderGame();
+    renderGame(gameStatus);
+    
     return {
         renderGame,
         gameStatus,
     }
 })();
+// gameBoard.gameStatus = [1,1,1,1,1,1,1,1,1]
+gameBoard.renderGame([1,1,1,1,1,1,1,1,1]);
