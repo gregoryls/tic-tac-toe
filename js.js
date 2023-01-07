77dc854ae9b7895a3ce5f0c0f6e8a4fa3f4236ea
Greg Schneider <greg53x@msn.com>
2023-01-07 13:55:16 -0700

    fix reset button removing itself

--- js.js
+++ js.js
@@ -161,12 +161,17 @@ const gameplay = (() => {
     }
 
     const deleteNode = (parentElementID,index) =>{
+        //TODO clean this up + add comments, don't remove if children is length 1
+        //TODO give a player name win message as well
+        //TD check to see if queryselectorall has the same whitespace problem
         parent = document.getElementById(`${parentElementID}`);
         children = parent.childNodes;
         let temp = [];
         for (let i = 0;i<children.length;i++){
             if (children[i].nodeType === 3) children[i].remove();
         }
+        //end function if only the reset button remains in the div
+        if (children.length < 2) return; 
         children[index].remove();
 
         // console.log(parent.firstChild)
